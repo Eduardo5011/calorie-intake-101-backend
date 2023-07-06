a4ee9cea4b45c35876e475ae1c81b04c80d09144
Eduardo5011 <eduardo.rosado5011@gmail.com>
2023-07-06 15:52:21 -0400

    update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,6 @@
 const allowedOrigins = [
     'https://www.calorie-intake-101.onrender.com',
-    // 'http://127.0.0.1:5500',
-    // 'http://localhost:3500',
-    // 'http://localhost:3000'
+   
 ];
 
 module.exports = allowedOrigins;
